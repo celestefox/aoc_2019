5636090e6642ab61245bbec96a7168f64c02eefc
Glowpelt <glowpelt@chakat.space>
2019-12-08 12:17:17 -0700

    feat: add d4, new helper function

---
+++ src/bin/day4.rs
@@ -0,0 +1,101 @@
+use aoc_2019::*;
+use std::error::Error;
+
+fn has_digit_pair(password: &String) -> bool {
+    let mut prev = ' ';
+    for char in password.chars() {
+        if char == prev {
+            return true;
+        }
+        prev = char;
+    }
+    false
+}
+
+fn has_unique_digit_pair(password: &String) -> bool {
+    //println!("str: `{}`", password);
+    let mut prev = ' ';
+    let mut iter = " "
+        .chars()
+        .chain(password.chars())
+        .zip(password.chars().chain(" ".chars()))
+        .peekable();
+    while let Some((c1, c2)) = iter.next() {
+        /*println!(
+            "p {} c1 {} c2 {} n {}",
+            prev,
+            c1,
+            c2,
+            iter.peek().unwrap_or(&('!', '!')).1
+        );*/
+        if c1 == c2 && c1 != prev {
+            if let Some((_, next)) = iter.peek() {
+                if c2 != *next {
+                    return true;
+                }
+            } else {
+                return true;
+            }
+        }
+        prev = c1;
+    }
+    false
+}
+
+fn digits_always_increase(password: &String) -> bool {
+    let mut prev = 0u8;
+    for byte in password.bytes() {
+        if byte < prev {
+            return false;
+        }
+        prev = byte;
+    }
+    true
+}
+
+// Preconditions: 6 digits, within the puzzle input range
+fn check_password_simple(password: usize) -> bool {
+    let string: String = password.to_string();
+    has_digit_pair(&string) && digits_always_increase(&string)
+}
+
+fn check_password_full(password: usize) -> bool {
+    let string = password.to_string();
+    has_unique_digit_pair(&string) && digits_always_increase(&string)
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let input = read_simple_input("input/day4.txt")?;
+    let mut iter = input.split('-').map(|n| n.parse::<usize>());
+    let start = iter.next().unwrap()?;
+    let end = iter.next().unwrap()?;
+    let mut valid_passwords: Vec<usize> = Vec::new();
+    for guess in start..=end {
+        if check_password_simple(guess) {
+            valid_passwords.push(guess);
+        }
+    }
+    println!("Day 4 part 1: {} possible passwords", valid_passwords.len());
+    let mut valid_passwords: Vec<usize> = Vec::new();
+    for guess in start..=end {
+        if check_password_full(guess) {
+            valid_passwords.push(guess);
+        }
+    }
+    println!("Day 4 part 2: {} possible passwords", valid_passwords.len());
+    Ok(())
+}
+
+#[test]
+fn test_check_password_simple() {
+    assert!(check_password_simple(111111));
+    assert!(!check_password_simple(223450));
+    assert!(!check_password_simple(123789));
+}
+
+#[test]
+fn test_check_password_full() {
+    assert!(check_password_full(112233));
+    assert!(!check_password_full(123444));
+    assert!(check_password_full(111122));
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,20 @@
-use std::io::{self, BufReader, prelude::*};
 use std::fs::File;
+use std::io::{self, prelude::*, BufReader};
 
 pub fn read_input(name: &str) -> io::Result<Vec<String>> {
     let file = File::open(name)?;
     let file = BufReader::new(file);
     Ok(file.lines().map(|x| x.unwrap()).collect())
+}
+
+pub fn read_simple_input(name: &str) -> io::Result<String> {
+    read_input(name)?
+        .first()
+        .ok_or(io::Error::new(
+            io::ErrorKind::Other,
+            "No first line in input",
+        ))
+        .map(|l| l.clone())
 }
 
 #[cfg(test)]
